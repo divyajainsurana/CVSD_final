747812ab3cea1947d2da8a9c4f61219d70c63c6b
divyajainsurana <43678200+divyajainsurana@users.noreply.github.com>
2020-01-19 18:25:26 +0800

    Add files via upload

---
+++ final_project_ST_v3/03_GATE/INCA_libs/irun.lnx86.15.20.nc/files.ts
@@ -0,0 +1,4 @@
+1579409717 /home/raid7_2/user07/r07158/finalproject/final_project_ST_v4/01_RTL/tb_v3.v
+1579408816 /home/raid7_2/user07/r07158/finalproject/final_project_ST_v4/03_GATE/MEMC_syn.v
+1575483352 /home/raid7_2/user07/r07158/finalproject/final_project_ST_v4/SRAM/sram_512x8/sram_512x8.vp
+1571602650 /home/raid7_2/user07/r07158/finalproject/final_project_ST_v4/03_GATE/tsmc13.v
